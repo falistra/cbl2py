fd9934c02285e2794e2cdea007a163dd740eed74
Fausto Zanasi <fausto.zanasi@gmail.com>
2022-11-17 18:47:28 +0100

    class types

---
+++ cbl2py/transpiler/data/types.py
@@ -0,0 +1,8 @@
+class Variable():
+    def __init__(self,level,name,picture,value):
+        self.level = level
+        self.name = name
+        self.picture = picture
+        self.value = value
+
+    
